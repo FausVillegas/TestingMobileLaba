c457b298539aff07b03920c1fb0d1ae3aff1da57
FausVillegas <faustovillegas2015@gmail.com>
2024-02-12 04:58:56 -0300

    Add iosHomePage class in src/main/java/com.zebrunner.carina.demo.gui.carinademo.ios without implementation for iOS devices

--- src/main/java/com/zebrunner/carina/demo/gui/carinademo/ios/iosHomePage.java
+++ src/main/java/com/zebrunner/carina/demo/gui/carinademo/ios/iosHomePage.java
@@ -1,2 +1,18 @@
-package com.zebrunner.carina.demo.gui.carinademo.ios;public class HomePage {
+package com.zebrunner.carina.demo.gui.carinademo.android;
+
+import com.zebrunner.carina.demo.gui.carinademo.common.HomePageBase;
+import com.zebrunner.carina.demo.gui.carinademo.common.LoginPageBase;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import org.openqa.selenium.WebDriver;
+
+@DeviceType(pageType = DeviceType.Type.IOS_PHONE, parentClass = HomePageBase.class)
+public class iosHomePage extends HomePageBase {
+    public iosHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public LoginPageBase clickNextBtn() {
+        throw new UnsupportedOperationException("Not implemented for iOS");
+    }
 }
